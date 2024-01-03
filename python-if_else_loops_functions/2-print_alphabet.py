a639076da5e6738db5ba25f89bb7c9f949de96e0
ThatsVie <Viepaulalmt@gmail.com>
2024-01-03 11:09:18 -0800

    trying different way

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,5 +2,5 @@
 # Loop through ASCII values for lowercase letters (97 to 122),
 # print each character using the format method, and prevent newlines.
 
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(i)), end="")
