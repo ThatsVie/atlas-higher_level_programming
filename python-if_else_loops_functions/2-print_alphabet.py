6b1ed0be32dcfa2e6926271685b75ff780ed2449
ThatsVie <Viepaulalmt@gmail.com>
2024-01-03 11:10:55 -0800

    deleting comments to see if it makes a difference because checkder is saying i have more than 1 loop but i dont

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# Loop through ASCII values for lowercase letters (97 to 122),
-# print each character using the format method, and prevent newlines.
-
 for i in range(ord('a'), ord('z') + 1):
     print("{:s}".format(chr(i)), end="")
