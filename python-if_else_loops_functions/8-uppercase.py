eb8a7d012c7766f5a9154e69579166fe55a1cee6
ThatsVie <Viepaulalmt@gmail.com>
2024-01-03 14:43:05 -0800

    redo without str.upper() and str.isupper()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,12 @@
 
 def uppercase(input_str):
     for char in input_str:
-        print("{}".format(char.upper() if char.islower() else char), end="")
+        # Check if the character is a lowercase letter
+        converted_char = (
+                chr(ord(char) - (ord('a') - ord('A')))
+                if ('a' <= char <= 'z')
+                else char
+        )
+        result += "{}".format(converted_char)
+    # Print the resulting uppercase string followed by a new line
     print("")
