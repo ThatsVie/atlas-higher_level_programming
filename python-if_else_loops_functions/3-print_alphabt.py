4b2865f888bd2a42fba7fab17f3431093d04847c
ThatsVie <Viepaulalmt@gmail.com>
2024-01-03 11:22:14 -0800

    correcting syntax

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) == 'e' or chr(i) == 'q':
         continue
-    else
-    print("{:s}".format(chr(i)), end="")
+    else:
+        print("{:s}".format(chr(i)), end="")
