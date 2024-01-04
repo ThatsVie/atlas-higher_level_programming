54b8712e758c159058175c02b9a1f5289ed7d86b
ThatsVie <Viepaulalmt@gmail.com>
2024-01-04 13:17:46 -0800

    Write a program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Check if the script is being executed directly
+if __name__ == "__main__":
+    # Import the variable 'a' from the module variable_load_5
+    from variable_load_5 import a
+
+    # Print the value of the imported variable 'a'
+    print(a)
