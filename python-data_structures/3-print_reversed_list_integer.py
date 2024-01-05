54dd2a0c6a139a22001af8bb8ef1ff90cb7b9f19
ThatsVie <Viepaulalmt@gmail.com>
2024-01-05 14:12:25 -0800

    indentation

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,9 +3,9 @@
 def print_reversed_list_integer(my_list=[]):
 # Check if the input list is None
     if my_list is None:
-    # Return None if the list is None
-    return None
-# Iterate through the list in reverse order
-for i in range(len(my_list), 0, -1):
-    # Print each integer using the str.format() method
-    print("{:d}".format(my_list[i - 1]))
+        # Return None if the list is None
+        return None
+    # Iterate through the list in reverse order
+    for i in range(len(my_list), 0, -1):
+        # Print each integer using the str.format() method
+        print("{:d}".format(my_list[i - 1]))
