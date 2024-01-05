8fba23becf9d3b445fa68a7fff976ea1774d777b
ThatsVie <Viepaulalmt@gmail.com>
2024-01-05 13:38:20 -0800

    Write a function that retrieves an element from a list.

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    # Check if the index is negative or greater/equal to the list length)
+    if idx < 0 or idx >= len(my_list):
+        # Return None if the index is out of range
+        return None
+    # Return the element at the specified index
+    return my_list[idx]
