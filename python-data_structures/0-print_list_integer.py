cd23a74148920eb67a24637ee129219376f24574
ThatsVie <Viepaulalmt@gmail.com>
2024-01-05 13:54:26 -0800

    commenting

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    # Iterate through each element (number) in the list
     for number in my_list:
+        # Print each integer using the str.format() method
         print("{:d}".format(number))
