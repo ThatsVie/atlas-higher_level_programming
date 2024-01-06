41fd82c1590d6a299f7529a34c00d6ecb3b68432
ThatsVie <Viepaulalmt@gmail.com>
2024-01-06 10:48:38 -0800

    indentation correction

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     # Use tuple slicing to ensure only the first 2 elements are considered
-     a = tuple_a[:2] + (0, 0)
-     b = tuple_b[:2] + (0, 0)
-     # Return a tuple with the sum of corresponding elements
-     return (a[0] + b[0], a[1] + b[1])
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+    # Return a tuple with the sum of corresponding elements
+    return (a[0] + b[0], a[1] + b[1])
