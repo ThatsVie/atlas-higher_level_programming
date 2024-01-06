363f082921016830975b1f5ec6e2b0912c07586f
ThatsVie <Viepaulalmt@gmail.com>
2024-01-06 11:01:35 -0800

    indentation correction

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-     if not sentence:  # Check if the string is empty
-         return (0, None)
-     # Return a tuple with the length and the first character of the string
-     return (len(sentence), sentence[0])
+    if not sentence:  # Check if the string is empty
+        return (0, None)
+    # Return a tuple with the length and the first character of the string
+    return (len(sentence), sentence[0])
