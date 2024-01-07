269c8fb65ea7ea771f0461dafb8db2fb87880e5c
ThatsVie <Viepaulalmt@gmail.com>
2024-01-07 15:08:31 -0800

    check for if string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Check if the input is an empty string
-    if not roman_string:
+    # Check if the input is None or not a string
+    if roman_string is None or type(roman_string) is not str:
+        # If the input is invalid, return 0
         return 0
     # Dictionary to map Roman numerals to integers
     roman_numerals = {
@@ -14,7 +15,7 @@ def roman_to_int(roman_string):
     # Iterate through the reversed string of Roman symbols
     for symbol in reversed(roman_string):
         # Retrieve value for current Roman symbol, default to 0 if not found
-        current_value = roman_numerals.get(symbol, 0)
+        current_value = roman_numerals[symbol]
 
         # Update the result based on Roman numeral rules
         if current_value < previous_value:
