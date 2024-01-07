2ad0ea4c433a51cad377c6e474bb4dfb8d2ab6c3
ThatsVie <Viepaulalmt@gmail.com>
2024-01-07 12:48:20 -0800

    Write a function that replaces or adds key/value in a dictionary.

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Update dictionary with new key-value pair
+    # or modify the value of an existing key
+    a_dictionary[key] = value
+    return a_dictionary
