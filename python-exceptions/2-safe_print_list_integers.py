aa83bbd677afda02d2e6030cafbf6b8063c01d36
ThatsVie <Viepaulalmt@gmail.com>
2024-01-08 11:30:50 -0800

    rearranging

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    # Initialize a counter for number of integers printed
     printed_integers = 0
-
-    try:
-        for i in range(x):
-            # Check if current element is an integer
-            if type(my_list[i]) == int:
-                # Print the integer value
-                print("{:d}".format(my_list[i]), end="")
-                printed_integers += 1
-        # Print a new line after all integers
-        print()
-    except (IndexError, ValueError):
-        # Handle cases where x is greater than the length of my_list
-        # or the value is not an integer
-        pass
+    # Iterate over the range from 0 to x-1
+     for i in range(x):
+        try:
+            # Attempt to print the current element as an integer
+            print("{:d}".format(my_list[i]), end="")
+            # Increment the counter for successfully printed integers
+            printed_integers += 1
+        except (ValueError, TypeError):
+            # Continue to next iteration if current element isnt an integer
+            continue
+    # Print a new line to separate the printed elements
+    print()
+    # Return the count of integers successfully printed
     return printed_integers
         
