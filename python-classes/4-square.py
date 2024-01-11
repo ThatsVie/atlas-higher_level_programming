3433ef6e35115bc2aff1006a4f2f48b728bf6822
ThatsVie <Viepaulalmt@gmail.com>
2024-01-11 15:36:45 -0800

    Write a class Square that defines a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Square class
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): Private attribute representing the size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance.
+
+        Parameters:
+            size (int): The size of the square. Default 0.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Parameters:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
