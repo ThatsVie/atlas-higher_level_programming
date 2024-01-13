3a01040545e4a522cdd5417e2ffeed38b084d79a
ThatsVie <Viepaulalmt@gmail.com>
2024-01-13 13:06:18 -0800

    deleted or float from error message

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,11 +20,11 @@ def add_integer(a, b=98):
     """
     # Check if a is an integer or float
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
 
     # Check if b is an integer or float
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+        raise TypeError("b must be an integer")
 
     # Cast a and b to integers
     a = int(a)
