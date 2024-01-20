6e8bf8e3686ac4439b961573474e35914ff23169
ThatsVie <Viepaulalmt@gmail.com>
2024-01-20 12:10:14 -0800

    adjustment

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,5 +7,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Public instance method that prints the list in ascending order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
