386baf4afea718b43f1f8245cac381622fe67b38
ThatsVie <Viepaulalmt@gmail.com>
2024-01-21 13:23:34 -0800

    square 2

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""11-square.py module - class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square, a special case of a rectangle with equal sides."""
+    def __init__(self, size):
+        """Initialize a square using the size parameter."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
