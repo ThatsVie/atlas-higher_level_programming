d2a421d5b435918ba2892a025fff2362cb70f632
ThatsVie <Viepaulalmt@gmail.com>
2024-01-22 13:09:55 -0800

    added end=

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""module - function reads a text file and prints to stdout"""
+"""Function reads a text file and prints to stdout"""
 
 
 def read_file(filename=""):
     """Read and print the content of a text file."""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
