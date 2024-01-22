bc50e35f680e02a2242edbf812a5019a2013af7b
ThatsVie <Viepaulalmt@gmail.com>
2024-01-22 13:12:16 -0800

    indentation

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
     filename (str): The name of the file.
     text (str): The text to append to the file.
     """
-     with open(filename, 'a', encoding='utf-8') as file:
-         return file.write(text)
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
