7ad42e47e3e9f7ca4dc1ea70b9a759225f4e551d
ThatsVie <Viepaulalmt@gmail.com>
2024-01-29 07:26:05 -0800

    tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -32,6 +32,66 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle.y, 7)
         self.assertEqual(rectangle.id, 123)
 
+    def test_invalid_width(self):
+        with self.assertRaises(TypeError):
+            Rectangle("invalid", 30, 7, 7)
+
+    def test_invalid_height(self):
+        with self.assertRaises(TypeError):
+            Rectangle(30, "invalid", 7, 7)
+
+    def test_invalid_x(self):
+        with self.assertRaises(TypeError):
+            Rectangle(15, 30, "invalid", 7)
+
+    def test_invalid_y(self):
+        with self.assertRaises(TypeError):
+            Rectangle(15, 30, 123, "invalid")
+
+    def test_negative_width(self):
+        with self.assertRaises(ValueError):
+            Rectangle(-4, 5)
+
+    def test_negative_height(self):
+        with self.assertRaises(ValueError):
+            Rectangle(4, -5)
+
+    def test_zero_width(self):
+        with self.assertRaises(ValueError):
+            Rectangle(0, 5)
+
+    def test_zero_height(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10, 0)
+
+    def test_negative_x(self):
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, -3, 4)
+
+    def test_negative_y(self):
+        with self.assertRaises(ValueError):
+            Rectangle(5, 10, 3, -6)
+
+    def test_calculate_area(self):
+        rec = Rectangle(8, 15)
+        result = rec.area()
+        self.assertEqual(result, 120)
+
+    def test_string_representation(self):
+        rec = Rectangle(8, 15, 4, 6, 987)
+        self.assertEqual(str(rec), "[Rectangle] (987) 4/6 - 8/15")
+
+    def test_display_without_position(self):
+        rec = Rectangle(5, 3)
+
+        output_buffer = StringIO()
+        sys.stdout = output_buffer
+
+        rec.display()
+
+        sys.stdout = sys.__stdout__
+
+        self.assertEqual(output_buffer.getvalue(), "#####\n#####\n#####\n")
 
 
 if __name__ == '__main__':
