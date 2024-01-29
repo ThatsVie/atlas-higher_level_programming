f41d6e8fa58351140a7c189d0b024dffc42c7401
ThatsVie <Viepaulalmt@gmail.com>
2024-01-29 04:37:37 -0800

    edit

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 """Unittests for base.py"""
 import unittest
-import json
+import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
 class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        # Remove any existing JSON files before each test
+        try:
+            os.remove("Rectangle.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove("Square.json")
+        except FileNotFoundError:
+            pass
+
+
     def test_auto_assign_id(self):
         obj = Base()
         self.assertEqual(obj.id, 1)
 
-    def test_auto_assign_id_incremented(self):
+    def test_auto_assign_id_multiple_instances(self):
         obj1 = Base()
         obj2 = Base()
-        self.assertEqual(obj1.id, 1)
-        self.assertEqual(obj2.id, 2)
+        self.assertEqual(obj1.id, obj2.id - 1)
 
     def test_save_id_passed(self):
         obj = Base(89)
@@ -30,7 +42,7 @@ class TestBase(unittest.TestCase):
         result = Base.to_json_string([])
         self.assertEqual(result, "[]")
 
-    def test_to_json_string_with_data(self):
+    def test_to_json_string_with_id(self):
         result = Base.to_json_string([{'id': 12}])
         self.assertEqual(result, '[{"id": 12}]')
 
@@ -42,7 +54,7 @@ class TestBase(unittest.TestCase):
         result = Base.from_json_string("[]")
         self.assertEqual(result, [])
 
-    def test_from_json_string_valid(self):
+    def test_from_json_string_with_id(self):
         result = Base.from_json_string('[{ "id": 89 }]')
         self.assertEqual(result, [{'id': 89}])
 
