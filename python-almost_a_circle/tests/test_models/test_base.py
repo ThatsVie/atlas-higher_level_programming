d8a71ba1197e5fc9f65f538127736ffbd8a06748
ThatsVie <Viepaulalmt@gmail.com>
2024-01-28 16:26:49 -0800

    added tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -25,5 +25,22 @@ class TestBase(unittest.TestCase):
         result = Base.to_json_string([{'id': 1, 'name': 'test'}])
         self.assertEqual(result, '[{"id": 1, "name": "test"}]')
 
+    def test_from_json_string_none(self):
+        result = Base.from_json_string(None)
+        self.assertEqual(result, [])
+
+    def test_from_json_string_empty(self):
+        result = Base.from_json_string("[]")
+        self.assertEqual(result, [])
+
+    def test_from_json_string_valid(self):
+        result = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(result, [{'id': 89}])
+
+    def test_from_json_string_valid_list(self):
+        result = Base.from_json_string('[{ "id": 89 }]')
+        self.assertIsInstance(result, list)
+        self.assertEqual(result[0]['id'], 89)
+
 if __name__ == '__main__':
     unittest.main()
