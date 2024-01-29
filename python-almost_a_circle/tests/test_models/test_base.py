36af4cc332ef915e25da05fe5545ee30b3622d2e
ThatsVie <Viepaulalmt@gmail.com>
2024-01-29 03:18:57 -0800

    tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 """Unittests for base.py"""
 import unittest
+import json
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    def test_init_with_id(self):
-        obj = Base(id=1)
+    def test_auto_assign_id(self):
+        obj = Base()
         self.assertEqual(obj.id, 1)
 
-    def test_init_without_id(self):
+    def test_auto_assign_id_incremented(self):
         obj1 = Base()
         obj2 = Base()
         self.assertEqual(obj1.id, 1)
         self.assertEqual(obj2.id, 2)
 
+    def test_save_id_passed(self):
+        obj = Base(89)
+        self.assertEqual(obj.id, 89)
+
+    def test_to_json_string_none(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
+
     def test_to_json_string_empty(self):
         result = Base.to_json_string([])
         self.assertEqual(result, "[]")
 
-    def test_to_json_string_non_empty(self):
-        result = Base.to_json_string([{'id': 1, 'name': 'test'}])
-        self.assertEqual(result, '[{"id": 1, "name": "test"}]')
+    def test_to_json_string_with_data(self):
+        result = Base.to_json_string([{'id': 12}])
+        self.assertEqual(result, '[{"id": 12}]')
 
     def test_from_json_string_none(self):
         result = Base.from_json_string(None)
@@ -41,16 +50,6 @@ class TestBase(unittest.TestCase):
         result = Base.from_json_string('[{ "id": 89 }]')
         self.assertIsInstance(result, list)
         self.assertEqual(result[0]['id'], 89)
-
-    def test_create_with_valid_arguments(self):
-        result = Base.create(id=1, name="test")
-        self.assertIsInstance(result, Base)
-        self.assertEqual(result.id, 1)
-        self.assertEqual(result.name, "test")
-
-    def test_create_with_invalid_arguments(self):
-        with self.assertRaises(AttributeError):
-            result = Base.create(id=1, width=2)
 
 if __name__ == '__main__':
     unittest.main()
