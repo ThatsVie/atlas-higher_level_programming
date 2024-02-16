f03448cd805df98b5bd2deb17c2059973e573458
ThatsVie <Viepaulalmt@gmail.com>
2024-02-16 15:57:58 -0800

    CREATE DATABASE IF NOT EXISTS hbtn_0d_2;

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,6 @@
 -- The user_0d_2 password set to user_0d_2_pwd
 -- Script does not fail if the database hbtn_0d_2 already exists
 -- Script does not dail if the user user_0d_2 already exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
