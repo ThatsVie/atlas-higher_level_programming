331a8a4e5af672cfad04bb8be84ecbd8a532b73b
ThatsVie <Viepaulalmt@gmail.com>
2024-02-16 11:24:59 -0800

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Creates a table called first_table in current database in MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- Will not fail if the table first_table already exists
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
