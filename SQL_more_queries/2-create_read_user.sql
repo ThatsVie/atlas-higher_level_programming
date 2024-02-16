0efdf2786123bb8a49d97c4bbd554d81f6cdcb80
ThatsVie <Viepaulalmt@gmail.com>
2024-02-16 15:54:55 -0800

    Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- The user_0d_2 password set to user_0d_2_pwd
+-- Script does not fail if the database hbtn_0d_2 already exists
+-- Script does not dail if the user user_0d_2 already exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
