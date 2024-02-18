17a807d64c154733c7568e76a69f202336240d81
ThatsVie <Viepaulalmt@gmail.com>
2024-02-18 15:24:37 -0800

    get a state

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -16,19 +16,19 @@ if __name__ == "__main__":
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
-    
+
     # Create tables in the database if they don't exist
     Base.metadata.create_all(db_engine)
 
     # Create a session maker bound to the engine
     Session = sessionmaker(bind=db_engine)
-    
+
     # Create a session to interact with the database
     db_session = Session()
 
     # Get the name of the state from the command-line argument
     state_name = sys.argv[4]
-    
+
     # Query the database to get the State object with the specified name
     state = db_session.query(State).filter(State.name == state_name).first()
 
