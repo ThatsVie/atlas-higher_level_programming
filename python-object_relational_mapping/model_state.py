ffac571ebb00762ef49cda01b458860c3d33d799
ThatsVie <Viepaulalmt@gmail.com>
2024-02-18 14:06:08 -0800

    pycodestyle validation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,12 +7,13 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create a declarative base instance
 Base = declarative_base()
 
+
 class State(Base):
     """Represents a state in the database."""
 
     # Table name in the database
     __tablename__ = 'states'
-    
+
     # Columns in the table
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
