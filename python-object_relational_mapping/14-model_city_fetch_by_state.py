2711d4b7494a385fa1d36f39b9dec53796dea02e
Vie <143755961+ThatsVie@users.noreply.github.com>
2024-02-18 20:41:54 -0600

    import sys

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,7 +2,7 @@
 """
 Prints all City objects from the database hbtn_0e_14_usa
 """
-from sys import argv
+import sys
 from model_state import State, Base
 from model_city import City
 from sqlalchemy import create_engine
