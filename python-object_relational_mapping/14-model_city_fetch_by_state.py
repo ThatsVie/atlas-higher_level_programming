18a0a782d2a04e042fa930c8e145ade90680c93c
ThatsVie <Viepaulalmt@gmail.com>
2024-02-18 17:13:57 -0800

    Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,21 +10,25 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     # Create an engine that connects to the database
-    db_engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}', pool_pre_ping=True)
+    db_engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}'
+                              f'@localhost:3306/{argv[3]}', pool_pre_ping=True)
     
     # Create the tables if they don't exist
     Base.metadata.create_all(db_engine)
-    
+
     # Create a session to interact with the database
     Session = sessionmaker(bind=db_engine)
     db_session = Session()
-    
+
     # Query the database to get all City objects along with their corresponding State names
     cities_with_states = db_session.query(City, State).join(State).all()
-    
+
     # Print each city along with its state name
     for city, state in cities_with_states:
         print(f'{state.name}: ({city.id}) {city.name}')
-    
+
     # Commit the changes
     db_session.commit()
+
+    # Close session
+    db_session.close()
