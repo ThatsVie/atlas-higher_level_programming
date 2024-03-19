ea7163c73b863b476d0967222d668cebba25b765
ThatsVie <viepaulalmt@gmail.com>
2024-03-19 07:05:50 -0500

    Write a script that prints JavaScript is amazing

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// This script is written in JavaScript and is intended to be
+// executed by the Node.js runtime.
+
+// Define a constant variable called myVar with the value
+// "JavaScript is amazing".
+const myVar = 'JavaScript is amazing';
+
+// Print the value of the variable myVar to the console.
+console.log(myVar);
