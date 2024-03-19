bd073a9288ac5fbafc1aabe1bb3d57e4435b8f2d
ThatsVie <viepaulalmt@gmail.com>
2024-03-19 07:54:09 -0500

    remember to use double space and not tab, single quote not double

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,14 +4,14 @@
 const numArgs = process.argv.length;
 
 // Check the number of arguments and print a message accordingly
-if (numArgs === 2) {
-	// If only the script name is passed (no arguments), print "No argument"
-	console.log("No argument");
+if (numArgs === 2) { 
+  // If only the script name is passed (no arguments), print 'No argument'
+  console.log('No argument');
 } else if (numArgs === 3) {
-	// If one argument is passed (including the script name itself),
-	// print "Argument found"
-	console.log("Argument found");
+  // If one argument is passed (including the script name itself),
+  // print 'Argument found'
+  console.log('Argument found');
 } else {
-	// If more than one argument is passed, print "Arguments found"
-	console.log("Arguments found");
+  // If more than one argument is passed, print 'Arguments found'
+  console.log('Arguments found');
 }
