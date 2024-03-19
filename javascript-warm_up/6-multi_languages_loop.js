3a7ffad05459c22af699e53262eaa80ccd17b0fd
ThatsVie <viepaulalmt@gmail.com>
2024-03-19 08:29:31 -0500

    Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Array containing the lines related to programming languages
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each line
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
