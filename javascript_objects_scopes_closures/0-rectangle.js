195fd8b87b442b0f00230e5aa17de70d37abdd2e
ThatsVie <viepaulalmt@gmail.com>
2024-03-22 16:20:34 -0500

    Write an empty class Rectangle that defines a rectangle

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// This line specifies the path to the Node.js interpreter,
+// allowing the script to be executed directly from the command line.
+
+class Rectangle {
+  // Define a class named Rectangle.
+  // Currently, it's an empty class without any properties or methods.
+}
+
+// Export the Rectangle class to make it available for use in other modules.
+module.exports = Rectangle;
