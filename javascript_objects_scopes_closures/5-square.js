1074c47a1f5baad2b767ac82698a7aed79fb9d42
ThatsVie <viepaulalmt@gmail.com>
2024-03-23 11:49:55 -0500

    troubleshooting why check for Correct output - size = -4 isnt working

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,22 @@
 #!/usr/bin/node
+
+// Importing the Rectangle class from the './4-rectangle' module.
 const Rectangle = require('./4-rectangle');
+
+// Defining the Square class that extends the Rectangle class.
 class Square extends Rectangle {
   constructor (size) {
+    // Calling the constructor of the parent class (Rectangle) using super().
+    // Passing the size argument to initialize both width and height attributes of the square.
     super(size, size);
+
+    // Validating that the size is a positive number.
+    if (size <= 0 || isNaN(size)) {
+      // If the size is not a positive number or is NaN, throw an error.
+      throw new Error('Size must be a positive number.');
+    }
   }
 }
 
+// Exporting the Square class to make it available for use in other modules.
 module.exports = Square;
