3787b0b1e79a367a5c97e10be37912adb3ada102
ThatsVie <viepaulalmt@gmail.com>
2024-03-23 12:01:12 -0500

    troubleshooting Correct output - size = -4

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,13 @@
 // imported from the './4-rectangle.js' module.
 module.exports = class Square extends require('./4-rectangle.js') {
   // Define the constructor function for the Square class.
-  constructor (size) {
+  constructor(size) {
+    if (size <= 0) {
+      // If size is less than or equal to 0, log 'undefined undefined' to the stdout and return.
+      console.log('undefined undefined');
+      return;
+    }
+    
     // Call the constructor of the parent class (Rectangle) using super().
     // Pass the size argument to initialize both width and height attributes of the square.
     super(size, size);
