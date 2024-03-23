f53a94f1730ffb0f75995b1b9476a134caa8a403
ThatsVie <viepaulalmt@gmail.com>
2024-03-23 12:21:40 -0500

    changed 4 to see if helps 5

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,39 +1,22 @@
 #!/usr/bin/node
 
-// This line specifies the path to the Node.js interpreter,
-// allowing the script to be executed directly from the command line.
-
 class Rectangle {
   constructor (w, h) {
     // Constructor function called when a new instance of Rectangle is created.
     // It takes two arguments: w (width) and h (height).
+    // If either width or height is not positive, return an empty object.
 
-    if (w > 0 && h > 0) {
-      // Check if both width and height are positive numbers.
-      // If so, initialize the width and height attributes with the given values.
-
-      this.width = w;
-      this.height = h;
-    } else {
-      // If either width or height is not a positive integer or equal to 0,
-      // create an empty rectangle with width and height set to 0.
-
-      this.width = 0;
-      this.height = 0;
+    if (w <= 0 || h <= 0) {
+      return {};
     }
+    this.width = w;
+    this.height = h;
   }
 
   print () {
     // Define a method named print for the Rectangle class.
     // This method prints the rectangle using the character 'X'.
 
-    if (this.width === 0 || this.height === 0) {
-      // If either width or height is 0, the rectangle is empty,
-      // so don't print anything.
-      return;
-    }
-
-    // Iterate over each row of the rectangle and print 'X' characters.
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
@@ -57,5 +40,4 @@ class Rectangle {
   }
 }
 
-// Export the Rectangle class to make it available for use in other modules.
 module.exports = Rectangle;
--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-// Import the Rectangle class from the './4-rectangle.js' module.
-const Rectangle = require('./4-rectangle.js');
+// Import the Rectangle class from the './4-rectangle' module.
+const Rectangle = require('./4-rectangle');
 
 // Define the Square class that extends the Rectangle class.
 class Square extends Rectangle {
@@ -10,12 +10,6 @@ class Square extends Rectangle {
     // Call the constructor of the parent class (Rectangle) using super().
     // Pass the size argument to initialize both width and height attributes of the square.
     super(size, size);
-
-    // Ensure size is a positive number
-    if (size <= 0) {
-      // Log 'undefined undefined' to stdout if size is not positive
-      console.log('undefined undefined');
-    }
   }
 }
 
